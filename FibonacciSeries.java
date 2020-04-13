c4ba05606698aa89e2df9a088044bf3d00565d34
Sohrab Khan <62949656+kodecaine@users.noreply.github.com>
2020-04-14 01:00:36 +0530

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,24 @@
+class Test 
+{ 
+    // Method to print first n Fibonacci Numbers 
+    static void printFibonacciNumbers(int n) 
+    { 
+        int f1 = 0, f2 = 1, i; 
+      
+        if (n < 1) 
+            return; 
+      
+        for (i = 1; i <= n; i++) 
+        { 
+            System.out.print(f2+" "); 
+            int next = f1 + f2; 
+            f1 = f2; 
+            f2 = next; 
+        } 
+    }  
+    // Driver Code 
+    public static void main(String[] args)  
+    { 
+        printFibonacciNumbers(7); 
+    } 
+}
